bd05c4e734262cef7e29ed972bffe26bc375948b
LBDCode <libby.duggan@gmail.com>
2019-07-27 20:22:22 -0400

    styled search bar

--- models/forms.py
+++ models/forms.py
@@ -4,10 +4,10 @@ from markupsafe import Markup
 from wtforms.widgets import HiddenInput
 
 class SearchForm(FlaskForm):
-    query = StringField('search')
-    style = SelectField(u'Beer Style', choices=[('', ''), ('brown', 'Brown'), ('ipa', 'IPA'), ('amber', 'Amber'), ('lager', 'Lager')])
-    abv = SelectField(u'ABV', choices=[('', ''), ('0-4', '<4'), ('4-6', '4-6'), ('6-8', '6-8'), ('8-10', '8-10'), ('>10', '>10')])
-    ibu = SelectField(u'IBU', choices=[('', ''), ('0-20', '<20'), ('21-40', '21-40'), ('41-60', '41-60'), ('61-80', '61-80'), ('81-100', '81-100'), ('>100', '>100')])
+    query = StringField('search', render_kw={"placeholder": "Search beer name or style"})
+    style = SelectField(u'Beer Style', choices=[('', 'Style'), ('brown', 'Brown'), ('ipa', 'IPA'), ('amber', 'Amber'), ('lager', 'Lager')])
+    abv = SelectField(u'ABV', choices=[('', 'ABV'), ('0.1-4', '<4'), ('4-6', '4-6'), ('6-8', '6-8'), ('8-10', '8-10'), ('>10', '>10')])
+    ibu = SelectField(u'IBU', choices=[('', 'IBU'), ('0.1-20', '<20'), ('21-40', '21-40'), ('41-60', '41-60'), ('61-80', '61-80'), ('81-100', '81-100'), ('>100', '>100')])
     submitAdvanced = SubmitField('Search')
 
 class SimpleSearchForm(FlaskForm):
